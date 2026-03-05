2449f603e1f639ea799cc5cefe7327d1b6c11679
Muyedur rahman <muyedrahman732@gmail.com>
2026-03-05 22:10:31 +0600

    add resolved task tadk status , ticketd card , cousnt box, count contaener

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,64 @@
-import './App.css'
-import CountContainer from './components/CountContainer';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import CountContainer from "./components/CountContainer";
+import { Suspense } from "react";
+import { ToastContainer } from "react-toastify";
+import Loading from "./components/Loading";
+
+
+
+const loadTickets = () => fetch("/tickets.json").then((res) => res.json());
 
 function App() {
- 
+  const ticketsPromise = loadTickets();
+  // console.log(ticketsPromise)
 
   return (
-    <>
+    <div className="">
       <header>
         <Navbar />
       </header>
 
       <section>
-        <CountContainer></CountContainer>
+        <Suspense fallback={<Loading />}>
+          <CountContainer promise={ticketsPromise}></CountContainer>
+        </Suspense>
       </section>
 
+      <ToastContainer />
+
       <Footer />
-    </>
+    </div>
   );
 }
 
-export default App
+export default App;
+
+// import './App.css'
+// import CountContainer from './components/CountContainer';
+// import Footer from './components/Footer';
+// import Navbar from './components/Navbar';
+
+// function App() {
+
+//   return (
+//     <>
+//       <header>
+//         <Navbar />
+//       </header>
+
+//       <section>
+//         <Suspense fallback={<Loading />}>
+//           <CountContainer promise={ticketsPromise}></CountContainer>
+//         </Suspense>
+//       </section>
+
+//       <ToastContainer />
+
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App
---
+++ src/components/cards/TaskStatus.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TaskStatus = ({ ticket, handleResolvedTask }) => {
+  return (
+    <div className="space-y-3 text-black bg-white p-4 rounded shadow">
+      <h2 className="font-bold">{ticket.title}</h2>
+      <button
+        onClick={() => handleResolvedTask(ticket)}
+        className="btn rounded-xl bg-green-500 text-white w-full"
+      >
+        Complete
+      </button>
+    </div>
+  );
+};
+
+export default TaskStatus;
